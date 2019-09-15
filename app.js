f43f9c442aea41106fa2fef3eb11746442b5c020
MJH <munjun.gun@gmail.com>
2019-09-15 23:58:27 +0900

    Initial Commit

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const canvas = document.getElementById("jsCanvas");
+const canvas = document.getElementById("jsCanvas"),
+    ctx = canvas.getContext("2d"),
+    colors = document.getElementsByClassName("jsColor");
+
+// 픽셀을 다루는 윈도우 크기를 지정
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = "2.5";
 
 let painting = false;
 
@@ -6,24 +15,37 @@ function stopPainting () {
     painting = false;
 }
 
-function onMouseUp() {
-    stopPainting();
-}
-
-function onMouseDown(event) {
+function startPainting() {
     painting = true;
 }
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 function init() {
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", stopPainting);
+    if(canvas) {
+        canvas.addEventListener("mousemove", onMouseMove);
+        canvas.addEventListener("mousedown", startPainting);
+        canvas.addEventListener("mouseup", stopPainting);
+        canvas.addEventListener("mouseleave", stopPainting);
+    }
+    Array.from(colors).forEach(color => 
+        color.addEventListener("click", handleColorClick)
+    );
 }
 
 init();
